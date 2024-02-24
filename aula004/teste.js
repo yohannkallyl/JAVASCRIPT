c540d368f36019b22aec5774d7ce08b00f42375d
yohannkallyl <kallylyohann@gmail.com>
2024-02-23 22:05:54 -0300

    Atualização
    avanços diários

---
+++ aula004/teste.js
@@ -0,0 +1,4 @@
+let n1 = Number(prompt("Digite um número: "))
+let n2 = Number(prompt("Digite outro número: ")) //conversão de tipos de variáveis
+let s = n1+n2
+alert(`O resultado da soma entre os números é : ${s}`)
---
+++ aula006/arquivo.js
@@ -0,0 +1,4 @@
+let nome = window.prompt("Digite o seu nome: ")
+document.write(`<b> Olá, ${nome}! Seu nome tem ${nome.length} caracteres<b> <br>`)
+document.write(`Seu nome em maiúsculas é ${nome.toUpperCase()} <br>`) // () -> método
+document.write(`Seu nome em minúsculas é ${nome.toLowerCase()} <br>`)
